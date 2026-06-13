3690910f898fb0fbeedb1f9037c6d5a2dd7b6f5d
athulrajk024 <athulrajk9746@gmail.com>
2026-06-13 20:41:56 +0530

    Remove all files

--- random.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main(){
-  srand(time(NULL));
-  int random = (rand()%2); //it will give random number 1 and 2
-  printf("%d",random);
-  return 0;
-} 
